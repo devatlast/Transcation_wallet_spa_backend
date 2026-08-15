8e89be67405f28615ce4f34c3ecad6c3a8a6834d
olawale <eolawale33@gmail.com>
2026-08-15 08:22:11 +0100

    fixing database

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first')
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+   user: process.env.DB_USER,
+   host: process.env.DB_HOST,
+   databse: process.env.DB_NAME,
+   password: process.env.DB_PASSWORD,
+   port: process.env.DB_PORT || 5432,
+   ssl: {
+    rejectUnauthorized: false
+   }
 });
 module.exports = pool;
