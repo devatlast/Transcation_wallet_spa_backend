371320069b13a129fdb0f53880eccc6c46147cf1
olawale <eolawale33@gmail.com>
2026-08-15 09:01:29 +0100

    Databse test

--- db.js
+++ db.js
@@ -4,11 +4,7 @@ dns.setDefaultResultOrder('ipv4first')
 const {Pool} = require('pg');
 
 const pool = new Pool({
-   user: process.env.DB_USER,
-   host: process.env.DB_HOST,
-   databse: process.env.DB_NAME,
-   password: process.env.DB_PASSWORD,
-   port: process.env.DB_PORT || 5432,
+   connectionString: process.env.DATABASE_URL,
    ssl: {
     rejectUnauthorized: false
    }
